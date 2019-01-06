0460bd5dc40152004292bb3a18011c98aed34fc4
Nagarajan N <nagarajan.natarajan@snapwiz.com>
2019-01-06 17:25:12 +0530

    added default export also

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import Box2dWorld,{Consumer} from './Box2dWorld';
 import Box2dObject from './Box2dObject';
 const World = Box2dWorld,WorldConsumer=Consumer,Item=Box2dObject;
 export {World,WorldConsumer,Item};
+
+export default {World,WorldConsumer,Item};
